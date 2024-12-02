b87b55e4c0217f5ae1d82787e5ebd5ccc5d14eef
David Frías <132804717+Adavidfr@users.noreply.github.com>
2024-12-02 10:59:09 -0500

    Update Parametros.py
    feat: se agrega parametros

--- Parametros.py
+++ Parametros.py
@@ -1,14 +1,46 @@
-#Parámetro Posicional
-def saludar(nombre, edad):
-    print(f"Hola {nombre}, tienes {edad} años")
-    
-saludar("David", 24)
+#Paranetro Posicional
+def saludar(nombre,edad):
+    print(f"Hola, {nombre}. tienes {edad} años.") 
 
-#Parámetro por defecto
-def descripcion(nom, ocupacion="Estudiante"):
+saludar("Pedro",24)
+
+#Parametro por defecto
+def descripcion(nom,ocupacion="Estudiantes"):
     print(f" {nom} es {ocupacion}")
     
+descripcion("Juan")
+descripcion("Peter","Ingeniero")    
     
-#Parámetros con palabras clave
+#Parametro con palabra clave
 
+def mostrar_info(nomb,ed,ciudad):
+    print(f"Nombre: {nomb}, Edad: {ed}, Ciudad: {ciudad}")
 
+mostrar_info(ed=29, ciudad="Quito", nomb="Marta")
+
+#parametro con Agumento  *Args  **Kawrgs 
+
+def sumar(*numeros):
+    total=sum(numeros)
+    print(f"La suma es: {total}")
+
+def mostrar_datos(**datos):
+    for clave,valor in datos.items():
+       print(f"{clave}: {valor}")
+    
+sumar(1,2,3,4,5,6,7,8,9)
+mostrar_datos(nombre="Laura", edad=15,ciudad="Quito")
+
+# Aqui se usa el Args
+def suma(*args):
+    return sum(args)
+
+print(suma(1,2,3))
+
+#Aqui se usa el Kwargs
+
+def mostrar_dato(**kwargs):
+    for clave, Value in kwargs.items():
+        print(f"{clave}, {Value}")
+        
+mostrar_dato(nombre="Michael", ciudad="Ibarra",edad=54,estatura=21,peso=23)
