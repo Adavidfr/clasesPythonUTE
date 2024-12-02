d7806879accd327d28b88f134f330c6797e10884
Adavidfr <adavidfruiz@gmail.com>
2024-12-02 09:21:35 -0500

    feat: add Parametros

---
+++ Parametros.py
@@ -0,0 +1,14 @@
+#Parámetro Posicional
+def saludar(nombre, edad):
+    print(f"Hola {nombre}, tienes {edad} años")
+    
+saludar("David", 24)
+
+#Parámetro por defecto
+def descripcion(nom, ocupacion="Estudiante"):
+    print(f" {nom} es {ocupacion}")
+    
+    
+#Parámetros con palabras clave
+
+
