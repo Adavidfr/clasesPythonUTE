8a4aa3da3b9e02a9d800c30cd657322eade8aefa
Alex David Frías <adavidfruiz@gmail.com>
2025-02-05 00:29:19 -0500

    feat: add advanced print usage examples in usoDePrintAvanzado.py

---
+++ investigacion/usoDePrintAvanzado.py
@@ -0,0 +1,23 @@
+print("Ejemplo de uso básico")
+nombre = "David"
+edad = 34
+print(f"Mi nombre es {nombre} y tengo {edad} años.")
+
+# Salida: Mi nombre es David y tengo 34 años.
+
+print("----------------------------------------------")
+
+print("Formateo con números")
+
+pi = 3.14159265
+print(f"El valor de pi con 3 decimales es {pi:.3f}")
+# Salida: El valor de pi con 3 decimales es 3.142
+
+print("----------------------------------------------")
+
+print("Incluir operaciones dentro de f-strings")
+
+num1, num2 = 10, 20
+print(f"La suma de {num1} y {num2} es {num1 + num2}")
+# Salida: La suma de 10 y 20 es 30
+
